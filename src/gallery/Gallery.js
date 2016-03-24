e4e983d3f25a6920be0577d4f0909ccec43bb734
Joseph Cortbawi <josephcortbawi@gmail.com>
2016-03-24 18:49:17 -0400

    Setup Gallery module structure

---
+++ src/gallery/Gallery.js
@@ -0,0 +1,15 @@
+/**
+ * @overview A module that initializes the Gallery with Flickity
+ * @module Gallery.js
+*/
+
+export const Gallery = {
+
+  /**
+   * Sets up any galleries on the page
+   * @returns {void}
+  */
+  init() {
+    console.log(`gallery init`);
+  },
+};

e8b6bc112ffa2b9ff4f3ea2f4b6f753e0a7cc25d
Stewart Yu <stewartyu@gmail.com>
2016-05-04 11:59:49 -0400

    Reset fixture with every test

--- src/accordion/Accordion.test.js
+++ src/accordion/Accordion.test.js
@@ -3,35 +3,37 @@ import jsdom from 'jsdom';
 
 import { Accordion } from './Accordion';
 
-global.document = jsdom.jsdom(`
-  <div id="accordion">
-    <ul class="accordion-list">
-      <li class="accordion-list-item">
-        <div class="accordion-section js-accordion-section">
-          <div class="accordion-section-toggle js-accordion-section-toggle">
-            <h4 class="accordion-section-toggle-content">Header</h4>
+test.beforeEach(t => {
+  global.document = jsdom.jsdom(`
+    <div id="accordion">
+      <ul class="accordion-list">
+        <li class="accordion-list-item">
+          <div class="accordion-section js-accordion-section">
+            <div class="accordion-section-toggle js-accordion-section-toggle">
+              <h4 class="accordion-section-toggle-content">Header</h4>
+            </div>
+            <ul class="accordion-section-list js-accordion-section-content">
+              <li class="accordion-section-list-item">
+                Content
+              </li>
+            </ul>
           </div>
-          <ul class="accordion-section-list js-accordion-section-content">
-            <li class="accordion-section-list-item">
-              Content
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li class="accordion-list-item">
-        <div class="accordion-section js-accordion-section">
-          <div class="accordion-section-toggle js-accordion-section-toggle">
-            <h4 class="accordion-section-toggle-content">Header</h4>
+        </li>
+        <li class="accordion-list-item">
+          <div class="accordion-section js-accordion-section">
+            <div class="accordion-section-toggle js-accordion-section-toggle">
+              <h4 class="accordion-section-toggle-content">Header</h4>
+            </div>
+            <ul class="accordion-section-list js-accordion-section-content">
+              <li class="accordion-section-list-item">
+                Content
+              </li>
+            </ul>
           </div>
-          <ul class="accordion-section-list js-accordion-section-content">
-            <li class="accordion-section-list-item">
-              Content
-            </li>
-          </ul>
-        </div>
-      </li>
-    </ul>
-  </div>`);
+        </li>
+      </ul>
+    </div>`);
+});
 
 // @todo: Write real tests
 test('Can initialize Accordion', t => {

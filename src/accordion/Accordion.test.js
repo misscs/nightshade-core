baa2661f44dcfa04d7725a3450177bf9f23ad40e
Stewart Yu <stewartyu@gmail.com>
2016-05-04 11:47:20 -0400

    Add jsdom for DOM testing

---
+++ src/accordion/Accordion.test.js
@@ -0,0 +1,46 @@
+import test from 'ava';
+import jsdom from 'jsdom';
+
+import { Accordion } from './Accordion';
+
+global.document = jsdom.jsdom(`
+  <div id="accordion">
+    <ul class="accordion-list">
+      <li class="accordion-list-item">
+        <div class="accordion-section js-accordion-section">
+          <div class="accordion-section-toggle js-accordion-section-toggle">
+            <h4 class="accordion-section-toggle-content">Header</h4>
+          </div>
+          <ul class="accordion-section-list js-accordion-section-content">
+            <li class="accordion-section-list-item">
+              Content
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="accordion-list-item">
+        <div class="accordion-section js-accordion-section">
+          <div class="accordion-section-toggle js-accordion-section-toggle">
+            <h4 class="accordion-section-toggle-content">Header</h4>
+          </div>
+          <ul class="accordion-section-list js-accordion-section-content">
+            <li class="accordion-section-list-item">
+              Content
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>`);
+
+// @todo: Write real tests
+test('Can initialize Accordion', t => {
+  Accordion.init('#accordion');
+  t.truthy(Accordion.accordion);
+  t.is(Accordion.transitionDuration, 300);
+});
+
+test('Can initialize Accordion transitionDuration', t => {
+  Accordion.init('#accordion', 100);
+  t.is(Accordion.transitionDuration, 100);
+});
--- src/accordion/test.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-
-import Accordion from './Accordion';
-
-// @todo: Write real tests
-test('Can initialize Accordion', t => {
-  t.true(true);
-});

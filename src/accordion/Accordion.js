4d7d1c89d29ad13f50537e3ba3a4ddcad7b2a3b9
Stewart Yu <stewartyu@gmail.com>
2016-05-09 17:43:48 -0400

    Fix Accordion linting errors

--- src/accordion/Accordion.js
+++ src/accordion/Accordion.js
@@ -3,6 +3,8 @@
  * @module Accordion.js
 */
 
+import * as velocity from 'velocity-animate';
+
 export const Accordion = {
 
   accordionSection : `.js-accordion-section`,
@@ -22,15 +24,15 @@ export const Accordion = {
     this.accordionSections = this.accordion.querySelectorAll(this.accordionSection);
 
     [...this.accordionSections].forEach((el) => {
-      let trigger = el.querySelector(this.accordionSectionTrigger);
+      const trigger = el.querySelector(this.accordionSectionTrigger);
 
       trigger.setAttribute(`touch-action`, `none`);
-      el.setAttribute(`aria-expanded`, false)
+      el.setAttribute(`aria-expanded`, false);
 
       trigger.addEventListener(`pointerup`, (e) => {
         e.stopPropagation();
 
-        let sectionContent = el.querySelector(this.accordionSectionContent);
+        const sectionContent = el.querySelector(this.accordionSectionContent);
 
         if (el.classList.contains(this.openClass)) {
           this.closeAccordionSection(el, sectionContent);
@@ -44,22 +46,24 @@ export const Accordion = {
   /**
    * Opens the given Accordion section
    * @param {object} section DOM element to toggle state class on
+   * @param {object} sectionContent DOM element to toggle animation on
    * @returns {void}
   */
   openAccordionSection(section, sectionContent) {
-    section.setAttribute(`aria-expanded`, true)
+    section.setAttribute(`aria-expanded`, true);
     section.classList.add(this.openClass);
-    Velocity(sectionContent, `slideDown`, this.transitionDuration);
+    velocity(sectionContent, `slideDown`, this.transitionDuration);
   },
 
   /**
    * Closes the given Accordion
    * @param {object} section DOM element to toggle state class on
+   * @param {object} sectionContent DOM element to toggle animation on
    * @returns {void}
   */
   closeAccordionSection(section, sectionContent) {
-    section.setAttribute(`aria-expanded`, false)
+    section.setAttribute(`aria-expanded`, false);
     section.classList.remove(this.openClass);
-    Velocity(sectionContent, `slideUp`, this.transitionDuration);
+    velocity(sectionContent, `slideUp`, this.transitionDuration);
   },
 };
